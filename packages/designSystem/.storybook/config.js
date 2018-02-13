03bb037a1b8b8f0152246a18552f43b2030352bf
Micah Godbolt <mgodbolt@microsoft.com>
2018-02-13 09:36:03 -0800

    include screener info

--- packages/designSystem/.storybook/config.js
+++ packages/designSystem/.storybook/config.js
@@ -1,3 +1,15 @@
+
+if (typeof window === 'object' && window.navigator && (/node\.js/i).test(window.navigator.userAgent)) {
+  let addons = require('@storybook/addons').default;
+  let Channel = require('@storybook/channels').default;
+  addons.setChannel(new Channel({
+    transport: {
+      setHandler: function() {},
+      send: function() {}
+    }
+  }));
+}
+
 import { configure } from '@storybook/vue'
 
 // automatically import all files ending in *.stories.js
@@ -7,3 +19,8 @@ function loadStories() {
 }
 
 configure(loadStories, module)
+
+
+if (typeof window === 'object') {
+  window.__screener_storybook__ = require('@storybook/vue').getStorybook();
+}
