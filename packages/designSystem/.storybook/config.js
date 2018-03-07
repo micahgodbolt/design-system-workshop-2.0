71ac784bf868c215795c8f2fa8f2795af73b6435
Micah Godbolt <mgodbolt@microsoft.com>
2018-03-06 21:48:25 -0800

    Move shell into components

--- packages/designSystem/.storybook/config.js
+++ packages/designSystem/.storybook/config.js
@@ -1,38 +1,42 @@
-
-if (typeof window === 'object' && window.navigator && (/node\.js/i).test(window.navigator.userAgent)) {
-  let addons = require('@storybook/addons').default;
-  let Channel = require('@storybook/channels').default;
-  addons.setChannel(new Channel({
-    transport: {
-      setHandler: function() {},
-      send: function() {}
-    }
-  }));
+if (
+  typeof window === "object" &&
+  window.navigator &&
+  /node\.js/i.test(window.navigator.userAgent)
+) {
+  let addons = require("@storybook/addons").default;
+  let Channel = require("@storybook/channels").default;
+  addons.setChannel(
+    new Channel({
+      transport: {
+        setHandler: function() {},
+        send: function() {}
+      }
+    })
+  );
 }
 
-import { configure, addDecorator } from '@storybook/vue'
-import Vue from 'vue';
+import { configure, addDecorator } from "@storybook/vue";
+import Vue from "vue";
 
-import Shell from '../src/utilities/Shell.vue';
+import Shell from "../src/components/Shell.vue";
 
 addDecorator(story => {
   const Story = story();
   return {
     components: { Story, Shell },
-    template: '<Shell style="padding: 30px; background: white" > <Story/> </Shell>'
-  }
-})
-
+    template:
+      '<Shell style="padding: 30px; background: white" > <Story/> </Shell>'
+  };
+});
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/components', true, /.stories.js$/);
+const req = require.context("../src/components", true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+  req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
 
-
-if (typeof window === 'object') {
-  window.__screener_storybook__ = require('@storybook/vue').getStorybook();
+if (typeof window === "object") {
+  window.__screener_storybook__ = require("@storybook/vue").getStorybook();
 }
