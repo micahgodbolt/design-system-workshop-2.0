e57cc390267b25a09e18b5af7cf43acc9dca0ce8
Micah Godbolt <mgodbolt@microsoft.com>
2018-03-13 21:58:16 -0700

    Added button

---
+++ packages/designSystem/src/components/Button/Button.stories.js
@@ -0,0 +1,23 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import {
+  withKnobs,
+  text,
+  number,
+  boolean,
+  array,
+  select,
+  color,
+  date
+} from '@storybook/addon-knobs';
+
+import myButton from './Button.vue';
+
+storiesOf('Button', module)
+  .addDecorator(withKnobs)
+  .add('Default', () => ({
+    components: { myButton },
+    template: `<my-button @click="click"> ${text('Text', "Button")} </my-button>`,
+    methods: { click: action('clicked') }
+  }))
+  ;
