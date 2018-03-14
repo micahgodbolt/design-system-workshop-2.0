929316da062a89d036fec164d41bfc993842aeda
Micah Godbolt <mgodbolt@microsoft.com>
2018-03-13 21:47:31 -0700

    Added link

---
+++ packages/designSystem/src/components/Link/Link.vue
@@ -0,0 +1,17 @@
+<template>
+  <a class="Link" :href="href"> <slot/> </a>
+</template>
+
+<script>
+export default {
+  props: ["href"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../utilities/utilities";
+
+.Link {
+  color: get-color(secondary);
+}
+</style>
