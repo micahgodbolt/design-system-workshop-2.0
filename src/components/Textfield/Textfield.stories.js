ca3824b9fe3501df6421b18a0a511440f7643538
Micah Godbolt <mgodbolt@microsoft.com>
2018-10-15 22:16:16 -0700

    05-exercise

--- src/components/Textfield/Textfield.stories.js
+++ src/components/Textfield/Textfield.stories.js
@@ -1,5 +1,6 @@
-import { storiesOf } from '@storybook/vue'
-import { action } from '@storybook/addon-actions'
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import * as Steps from 'screener-runner/src/steps';
 import {
   withKnobs,
   text,
@@ -9,26 +10,52 @@ import {
   select,
   color,
   date
-} from '@storybook/addon-knobs'
+} from "@storybook/addon-knobs";
 
-import Textfield from './Textfield.vue'
+import Textfield from "./Textfield.vue";
 
-storiesOf('Textfield', module)
+storiesOf("Textfield", module)
   .addDecorator(withKnobs)
-  .add('New', () => ({
+  .addDecorator((storyFn) => {
+    const story = storyFn();
+    story.steps = new Steps()
+      .snapshot('default', { cropTo: '.Textfield' })
+      .hover('.Textfield')
+      .snapshot('hover', { cropTo: '.Textfield' })
+      .setValue('input', 'Enter Text In Input')
+      .snapshot('text', { cropTo: '.Textfield' })
+      .end()
+    return story;
+  })
+  .add("New", () => ({
     components: { Textfield },
     template: `
       <Textfield 
-        placeholder="Put text in here" 
+        @input="input" 
+        @enter="enter" 
+        @blur="blur" 
+        placeholder="${text('placeholder', 'Put text in here')}" 
         variant="new"
       />`,
+    methods: {
+      input: action("change"),
+      enter: action("enter"),
+      blur: action("blur")
+    }
   }))
-  .add('Edit', () => ({
+  .add("Edit", () => ({
     components: { Textfield },
     template: `
       <Textfield 
-        placeholder="Put text in here" 
+        @input="input" 
+        @enter="enter" 
+        @blur="blur" 
+        placeholder="${text('placeholder', 'Put text in here')}" 
         variant="edit"
       />`,
-  }))
-  ;
+    methods: {
+      input: action("change"),
+      enter: action("enter"),
+      blur: action("blur")
+    }
+  }));
